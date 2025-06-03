bd048f93044c29f5d2480a7c7e4b990870acac03
liao-046 <ritaliao2003@gmail.com>
2025-06-03 19:28:27 +0800

    補充 schema.sql

--- schema.sql
+++ schema.sql
@@ -2,11 +2,19 @@ CREATE DATABASE IF NOT EXISTS book_system;
 USE book_system;
 
 -- 接下來加上建立資料表的語法
-CREATE TABLE User (
+CREATE TABLE user(
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50),
     email VARCHAR(100) UNIQUE,
     password VARCHAR(255)
 );
 
-
+CREATE TABLE book(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(200) NOT NULL, 
+    author VARCHAR(100),
+    publisher VARCHAR(100),
+    category VARCHAR(50),
+    cover_url VARCHAR(255),
+    description TEXT
+);
